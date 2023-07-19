306f32de67dd0267ee29379bc47cbd5de2cc184e
lorran <lorranazevedo153@gmail.com>
2023-07-18 21:58:27 -0300

    tentativa de função apagar

--- script.js
+++ script.js
@@ -2,79 +2,151 @@
 // maca += 3;
 // 11
 
+
+
+//criando um array de objetos. cada objeto tem 3 propriedades, cada propriedade tem um valor
 let produtos =
-    [
-        { id: 1, nome: 'sapato', preco: 100.00 },
-        { id: 2, nome: 'sandalia', preco: 250.00 },
-        { id: 3, nome: 'cinto', preco: 450.00 },
-        { id: 4, nome: 'bolsa', preco: 650.00 }
-    ]
+[
+    { id: 1, nome: 'sapato', preco: 100.00 },
+    { id: 2, nome: 'sandalia', preco: 250.00 },
+    { id: 3, nome: 'cinto', preco: 450.00 },
+    { id: 4, nome: 'bolsa', preco: 650.00 }
+]
 
 
+
+//crio uma variável tabela para trazer o elemento table utilizando o id dela
 const tabela = document.querySelector('#tabela');
-getprodutos();
+limpaTabela();
+getprodutos(); 
+
+
 
 function getprodutos() 
 {
-    produtos.forEach((produto) => {
+//esta função irá listar os produtos na tabela, utilizando manipulação do DOM
 
-        tabela.innerHTML += `<tr id="a${produto.id}">`;
+//loop mais externo forEach: irá percorrer os produtos
+produtos.forEach((produto) => {
+    
+    //cria uma linha na tabela para começar a inserir os dados do produto
+    tabela.innerHTML += `<tr id="a${produto.id}">`;
 
-        for (let propriedade in produto) {
-            document.querySelector("#" + 'a' + produto.id).innerHTML += `<td> ${produto[propriedade]} </td>`;
-        }
+    //loop mais interno for: irá percorrer as propriedades do produto "da vez"    
+    for (let propriedade in produto) {
+        //irá inserir na tabela cada um dos valores de cada uma das propriedades daquele produto
+        document.querySelector("#" + 'a' + produto.id).innerHTML += `<td> ${produto[propriedade]} </td>`;
+    }
+    //inserindo um botão de DELETE
+    // let btnDeletar = `<td><button class=btn-apagar" onclick="delete({produto.id})">APAGAR</td>`
 
-        tabela.innerHTML += "</tr>";
-    });
+    //primeiro criamos o BOTAO, aplicamos as propriedades e depois o criamos um TD pra ele                        
+    let botaoApagar = document.createElement('button');
+    botaoApagar.setAttribute('class', 'btn-apagar');
+    botaoApagar.setAttribute('onclick', 'apagarProduto()');
+    botaoApagar.innerHTML = 'APAGAR'
+
+    //inserindo um botão de UPDATE:                
+    // primeiro criamos o BOTAO, aplicamos as propriedades e depois o criamos um TD pra ele                        
+    let botaoEditar = document.createElement('button');
+    botaoEditar.setAttribute('class', 'btn-editar');
+    botaoEditar.setAttribute('onclick', 'edit({produto.id})');
+    botaoEditar.innerHTML = 'EDITAR';
+
+    //criando TDs e anexando a TR
+    let td = document.createElement('td');                
+    
+    td.appendChild(botaoEditar);
+    td.appendChild(botaoApagar);
+
+    tr = document.querySelector("#" + 'a' + produto.id)   ;
+    tr.appendChild(td);
+
+    //fecha a linha da tabela
+    tabela.innerHTML += "</tr>";
+});
 }
+
 
 
 function cadastrar() 
 {
-    //PASSO 1: pegando as informações que o usuario digitou e colocando em variaveis
-    const idInsere = document.querySelector('#id').value;
-    const nomeInsere = document.querySelector('#nome').value;
-    const precoInsere = document.querySelector('#preco').value;
+//Esta função irá cadastrar um novo produto no array de produtos
 
-    //PASSO 2: criando um objeto produto 
-    let produto = 
-    {
-        id: idInsere,
-        nome: nomeInsere,
-        preco: precoInsere
-    }
+//PASSO 1: pegando as informações que o usuario digitou e colocando em variaveis
+const idInsere = document.querySelector('#id').value;
+const nomeInsere = document.querySelector('#nome').value;
+const precoInsere = document.querySelector('#preco').value;
 
-    //PASSO 3: inserindo o objeto PRODUTO no vetor PRODUTOS
-    produtos.push(produto);
-
-    //PASSO4: inserindo os elementos na tabela com INNERHTML
-    tabela.innerHTML += `<tr id="a${produto.id}">`;
-
-    document.querySelector("#" + 'a' + produto.id).innerHTML += `<td> ${produto.id} </td>`;
-    document.querySelector("#" + 'a' + produto.id).innerHTML += `<td> ${produto.nome} </td>`;
-    document.querySelector("#" + 'a' + produto.id).innerHTML += `<td> ${produto.preco} </td>`;
-
-    tabela.innerHTML += "</tr>";
-}
-
-
-// function apagar()
-// {
-//     let idSelecionado = querySelector('#remover').value;
-
-//     if(idSelecionado == idInsere)
-//     {
-//         tabela.remove(produtos[idSelecionado]);
-//     }
-
-// }   
-
-
-function apagar()
+//PASSO 2: criando um objeto produto 
+let produto = 
 {
-    let idSelecionado = querySelector('#remover').value;
-    let tabelaSelecionada = querySelector('#tabela');
-
-    // remover inner html
-        
+    id: idInsere,
+    nome: nomeInsere,
+    preco: precoInsere
 }
+
+//PASSO 3: inserindo o objeto PRODUTO no vetor PRODUTOS
+produtos.push(produto);
+
+limpaTabela();
+getprodutos();    
+
+}
+
+
+
+function mostraCadastro() 
+{
+//mostra ou oculta a tela de cadastro quando o usuário clica no botão abaixo da tabela
+const div = document.getElementById('div-cadastrar');
+if (div.classList.contains('div-cadastrar-inativo')) {
+    div.classList.remove('div-cadastrar-inativo');
+    div.classList.add('div-cadastrar-ativo');
+}
+else {
+    div.classList.remove('div-cadastrar-ativo');
+    div.classList.add('div-cadastrar-inativo');
+}
+
+}
+
+
+
+function limpaTabela() 
+{     
+//zera a tabela e deixa só o cabeçalho
+ tabela.innerHTML = "<tr> <th>ID</th> <th>PRODUTO</th> <th>PREÇO</th>  <th>AÇÕES</th> </tr>" ;
+}
+
+
+
+function apagarProduto()
+{
+    produtos.forEach((produto) => {
+
+        // let tableSelecionada = document.querySelector('#tabela');
+        let idSelecionado = produto;
+
+        delete innerHTML(idSelecionado);
+    });
+
+    
+}
+
+// produtos.forEach((produto) => {
+
+        
+    //     let tableSelecionada = document.querySelector('#tabela');
+    //     let idSelecionado = produto;
+
+    //     let deletor = tableSelecionada.childNodes[idSelecionado];
+    //     tableSelecionada.removeChild(deletor);
+
+    // });
+
+
+    // delete produtos.id;
+    // delete produtos.nome;
+    // delete produtos.preco;    
+
